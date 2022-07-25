bc4c591f8519fd35eae574da16eaf2a6cc736767
bs2k <bs2k@bs2k.me>
2022-07-25 15:03:34 +0900

    sdsdfafda

--- export.sh
+++ export.sh
@@ -7,4 +7,5 @@ mkdir -p ~/.local/share/godot/templates/
 unzip -d ~/.local/share/godot/templates/ build/templates.zip
 mv ~/.local/share/godot/templates/templates ~/.local/share/godot/templates/3.4.4.stable
 chmod +x ./build/godot/Godot_v3.4.4-stable_linux_headless.64
+mkdir build/built
 ./build/godot/Godot_v3.4.4-stable_linux_headless.64 -v --export "HTML5" build/built/index.html
